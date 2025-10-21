ae378c2d1b86c45a041b3a2c6c31071f27e94d6e
pramthawatchai <pramthawatchay.40170@gmail.com>
2025-10-21 13:41:59 +0700

    update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask#, jsonify, request
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1 style='font-size: 14px; color: blue;'>Hello, World!</h1>"
 
 # @app.route("/json", methods=['GET'])
 # def json():
@@ -18,5 +18,6 @@ def hello_world():
 @app.route("/json")
 def json():
     return {
-        "code": "007"
+        "code": "007",
+        "name": "test"
     }
